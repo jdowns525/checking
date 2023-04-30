cea587e7d1e5c1d03ab507e30e1eee5118e7c43a
Josseph Downs <downsjosseph@gmail.com>
2023-04-30 18:54:46 +0000

    added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
+  get 'likes/create'
+  get 'likes/destroy'
+  get 'comments/create'
+  get 'comments/destroy'
+  get 'users/show'
   devise_for :users
 
   resources :users, only: [:show] do
