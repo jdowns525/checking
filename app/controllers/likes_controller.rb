0c2c2f54f1d8aaa04b74f1c2a85f11a3141de5b1
Josseph Downs <downsjosseph@gmail.com>
2023-04-30 19:16:36 +0000

    added more changes to the controllers

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,29 @@
 class LikesController < ApplicationController
+  before_action :set_post
+
   def create
+    @like = @post.likes.build(user: current_user)
+
+    if @like.save
+      redirect_to @post, notice: 'Liked successfully.'
+    else
+      redirect_to @post, alert: 'Unable to like post.'
+    end
   end
 
   def destroy
+    @like = @post.likes.find_by(user: current_user)
+
+    if @like&.destroy
+      redirect_to @post, notice: 'Unliked successfully.'
+    else
+      redirect_to @post, alert: 'Unable to unlike post.'
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :users, only: [:show] do
+  resources :users do
     member do
       get :following, :followers
     end
   end
 
   resources :posts do
-    resources :comments, only: [:create, :destroy]
+    resources :comments, except: [:index, :show]
     resources :likes, only: [:create, :destroy]
   end
 
   resources :relationships, only: [:create, :destroy]
-  
+
   root 'posts#index'
 end
