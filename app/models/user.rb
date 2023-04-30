34ac3dfa277df5cdb1f98bdf45f2b41ca6da6bc7
Josseph Downs <downsjosseph@gmail.com>
2023-04-30 18:45:10 +0000

    added relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
+  has_one_attached :profile_picture
+
+  has_many :active_relationships, class_name: "Relationship",
+                                 foreign_key: "follower_id",
+                                 dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show] do
+    member do
+      get :following, :followers
+    end
+  end
+
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy]
+  end
+
+  resources :relationships, only: [:create, :destroy]
+  
+  root 'posts#index'
 end
